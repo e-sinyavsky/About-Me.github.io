b4865aeef4fe8e209ef807de83b0c09af87b886b
e-sinyavsky <lokaloka5489318@gmail.com>
2025-06-20 14:41:41 +0300

    added to about me sections

---
+++ scripts/theme-toggle.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('themeToggle');
+    const theme = localStorage.getItem('theme') || 'light';
+
+    document.documentElement.setAttribute('data-theme', theme);
+    themeToggle.setAttribute('data-theme', theme);
+
+    themeToggle.addEventListener('click', () => {
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+        document.documentElement.setAttribute('data-theme', newTheme);
+        themeToggle.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    })
+})
