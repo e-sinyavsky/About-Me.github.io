05815fafe5f39abb064089b15d5ef85e9a43133a
e-sinyavsky <lokaloka5489318@gmail.com>
2025-06-21 14:03:38 +0300

    fix some ui bugs, and use prettier for all files

--- scripts/burger.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".header__burger").addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.querySelector(".header__menu").classList.toggle("open");
-});
